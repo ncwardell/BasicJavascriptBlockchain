07f132031d24c98bed92aba5062e60fd318f25d7
Noah <41527027+ncwardell@users.noreply.github.com>
2022-12-26 18:54:47 -0500

    Create blockchain.js

---
+++ blockchain.js
@@ -0,0 +1,51 @@
+class Block {
+  constructor(index, previousHash, timestamp, data, hash) {
+    this.index = index;
+    this.previousHash = previousHash.toString();
+    this.timestamp = timestamp;
+    this.data = data;
+    this.hash = hash.toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "0", Date.now(), "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = this.calculateHash(newBlock);
+    this.chain.push(newBlock);
+  }
+
+  calculateHash(block) {
+    const { index, previousHash, timestamp, data } = block;
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== this.calculateHash(currentBlock)) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
